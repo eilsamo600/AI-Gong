43a0dfb2a6d3c11866c3c6ee6f0a1dc5a65039fe
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-04-13 16:24:32 +0900

    add classroom-service

---
+++ backend/classroom-service/src/main/java/gcu/backend/classroomservice/ClassroomServiceApplication.java
@@ -0,0 +1,15 @@
+package gcu.backend.classroomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@enableDiscoveryClient
+public class ClassroomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomServiceApplication.class, args);
+	}
+
+}
---
+++ backend/classroom-service/src/test/java/gcu/backend/classroomservice/ClassroomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gcu.backend.classroomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
