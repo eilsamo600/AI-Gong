f934694f473837740c40f881312d71408a8382be
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-04-14 03:20:54 +0900

    add reservation-service

---
+++ backend/reservation-service/src/main/java/gcu/backend/reservationservice/controller/ReservationController.java
@@ -0,0 +1,19 @@
+package gcu.backend.reservationservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@Tag(name = "Reservation", description = "예약 API")
+public class ClassroomControler {
+    @GetMapping("/test2")
+    @Operation(summary = "테스트2", description = "테스트2")
+    public ResponseEntity<String> show() {
+        return ResponseEntity.ok("my name is incubator!");
+    }
+}
