5886bd785d7dee7e1d9c484d70c41065d20c2d2c
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-05-05 18:45:52 +0900

    add login page

--- frontend/ai_gong/lib/routes.dart
+++ frontend/ai_gong/lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:ai_gong/pages/dev_route/view/route_view_page.dart';
+import 'package:ai_gong/pages/login/view/login_view_page.dart';
 import 'package:ai_gong/pages/main/view/main_view_page.dart';
 import 'package:ai_gong/pages/my_info/view/my_info_view_page.dart';
 import 'package:ai_gong/pages/search/view/search_view_page.dart';
@@ -12,6 +13,7 @@ class CustomRouter {
     ),
     GetPage(name: MainViewPage.url, page: () => const MainViewPage()),
     GetPage(name: MyInfoViewPage.url, page: () => const MyInfoViewPage()),
-    GetPage(name: SearchViewPage.url, page: () => const SearchViewPage())
+    GetPage(name: SearchViewPage.url, page: () => const SearchViewPage()),
+    GetPage(name: LoginViewPage.url, page: () => const LoginViewPage())
   ];
 }
