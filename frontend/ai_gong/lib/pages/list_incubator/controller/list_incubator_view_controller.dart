b19b31205cf1a65b3aff3c731868619298a73ca7
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-05-06 02:43:41 +0900

    update reservation

--- frontend/ai_gong/lib/pages/list_incubator/controller/list_incubator_view_controller.dart
+++ frontend/ai_gong/lib/pages/list_incubator/controller/list_incubator_view_controller.dart
@@ -1,13 +1,112 @@
-import 'package:get/get.dart';
 import 'package:ai_gong/common/service_response.dart';
 import 'package:ai_gong/restAPI/api_service.dart';
 import 'package:ai_gong/restAPI/models/Classroom.dart';
 import 'package:ai_gong/restAPI/response/get_classroom_list_response.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ListIncubatorViewController extends GetxController {
-  static ListIncubatorViewController get instance =>
-      Get.find<ListIncubatorViewController>();
+  static ListIncubatorViewController get instance => Get.find<ListIncubatorViewController>();
+
+  RxList<int> states = List.filled(17, 0).obs;
+
+  void statesInit() {
+    states.value = List.filled(16, 0);
+    states.refresh();
+  }
+
+  void selected(int index) {
+    int start = states.value.indexOf(1);
+    int end = 0;
+    if (states.value[index] == 0) {
+      if (start == -1) {
+        // 아무것도 안 눌림
+        states.value[index] = 1;
+
+        for (int i = index + 4; i < states.length; i++) {
+          if (i < states.length) {
+            states.value[i] = 2;
+          }
+        }
+        for (int i = index - 1; i >= 0; i--) {
+          states.value[i] = 2;
+        }
+      } else {
+        // 무언가 눌림
+        for (int i = start; i < index + 1; i++) {
+          states.value[i] = 1;
+        }
+      }
+    } else if (states.value[index] == 1) {
+      for (int i = index; i < start + 4; i++) {
+        states.value[i] = 0;
+      }
+      if (!states.value.contains(1)) {
+        for (int i = 0; i < states.length; i++) {
+          states.value[i] = 0;
+        }
+      }
+      // states.value.
+      // if (!states.value.contains(1)) {
+      //   for (int i = 0; i < states.length; i++) {
+      //     states.value[i] = 0;
+      //   }
+      // }
+    }
+    // 취소
+    // if (states.value[index] == 1) {
+    //   states.value[index] = 0;
+    // } else if (states.value[index] == 0) {
+    //   states.value[index] = 1;
+    // }
+
+    // if (states.value[index] == 1) {
+    //   states.value[index] = 0;
+    //   int start = states.value.indexOf(1);
+    //   // 선택한 애 앞에 3개는 다 흰색
+    //   if (start != 0) {
+    //     for (int i = start + 1; i < (start + 4 > states.length ? states.length : start + 4); i++) {
+    //       if (start < index + 4) {
+    //         if (states.value[i] != 1) {
+    //           states.value[i] = 0;
+    //         }
+    //       }
+    //     }
+    //     for (int i = start - 1; i >= 0; i--) {
+    //       states.value[i] = 2;
+    //     }
+    //   }
+    //   start = states.value.indexOf(1);
+    //   if (start == -1) {
+    //     for (int i = 0; i < states.length; i++) {
+    //       states.value[i] = 0;
+    //     }
+    //   }
+    //   start = states.value.indexOf(1);
+    //   for (int i = index + 1; i < (index + 3 > states.length ? states.length : index + 3); i++) {
+    //     if (states.value[i] == 1) {
+    //       states.value[i] = 0;
+    //     }
+    //   }
+    // } else if (states.value[index] == 0) {
+    //   // 선택
+    //   states.value[index] = 1;
+    //   // 무조건 내뒤는 다 검정색
+    //   int start = states.value.indexOf(1);
+    //   if (start != 0) {
+    //     for (int i = start - 1; i >= 0; i--) {
+    //       states.value[i] = 2;
+    //     }
+    //   }
+
+    //   // 선택한 애 앞에 다 검정색
+    //   for (int i = index + 4; i < states.length; i++) {
+    //     states.value[i] = 2;
+    //   }
+    // }
+
+    states.refresh();
+  }
 
   @override
   void onInit() async {
@@ -21,8 +120,7 @@ class ListIncubatorViewController extends GetxController {
   }
 
   Future<void> getClassRoomList() async {
-    ApiResponse<ClassRoomListResponse> response =
-        await ApiService.instance.getClassRoomList();
+    ApiResponse<ClassRoomListResponse> response = await ApiService.instance.getClassRoomList();
     if (response.result) {
       classRoomList.value = response.value!.classrooms!;
     }
