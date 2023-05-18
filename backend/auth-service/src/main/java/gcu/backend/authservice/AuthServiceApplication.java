0c2a48c061daefc537904972b5c97a8f04948d75
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-05-18 18:26:59 +0900

    update auth repository error

--- backend/auth-service/src/main/java/gcu/backend/authservice/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/gcu/backend/authservice/AuthServiceApplication.java
@@ -3,10 +3,8 @@ package gcu.backend.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "gcu.backend")
 @EnableDiscoveryClient
 public class AuthServiceApplication {
 
