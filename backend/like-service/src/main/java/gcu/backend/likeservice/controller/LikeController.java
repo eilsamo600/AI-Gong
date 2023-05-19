4ecc67c9cd66b74f676e792d2c16eb0e645edc0a
bamb14 <althcjstk08@gachon.ac.kr>
2023-05-19 19:01:40 +0900

    update like-service

--- backend/like-service/src/main/java/gcu/backend/likeservice/controller/LikeController.java
+++ backend/like-service/src/main/java/gcu/backend/likeservice/controller/LikeController.java
@@ -14,8 +14,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.core.Response;
+import gcu.backend.likeservice.model.Incubator;
 import gcu.backend.likeservice.model.Like;
 import gcu.backend.likeservice.repository.LikeRepository;
+import gcu.backend.likeservice.repository.IncubatorRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -23,14 +25,14 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 
 @RestController
-@Tag(name = "Like", description = "즐겨찾기")
+@Tag(name = "Like", description = "즐겨찾기 API")
 public class LikeController {
 
     @Autowired
     private LikeRepository likeRepository;
 
     @PostMapping("/like")
-    @Operation(summary = "즐겨찾기 내역 보내기", description = "즐겨찾기 내역 보내요~.")
+    @Operation(summary = "예약 내역 보내기", description = "예약 내역 보내요~.")
     public ResponseEntity<Like> postLike(@Valid @RequestBody Like like) {
         System.out.print(like.toString());
         Like savedLike = likeRepository.save(like);
@@ -38,21 +40,25 @@ public class LikeController {
 
     }
 
-    @GetMapping("/likes")
-    @Operation(summary = "모든 즐겨찾기 테이블 조회", description = "즐겨찾기 테이블 정보입니다.")
-    public ResponseEntity<List<Like>> getLikes() {
-        List<Like> likes = likeRepository.findAll();
-        return new ResponseEntity<List<Like>>(likes, HttpStatus.OK);
-    }
+    // @GetMapping("/likes")
+    // @Operation(summary = "모든 예약 테이블 조회", description = "예약 테이블 정보입니다.")
+    // public ResponseEntity<List<Reservation>> getReservations() {
+    // List<Reservation> reservations = reservationRepository.findAll();
+    // return new ResponseEntity<List<Reservation>>(reservations, HttpStatus.OK);
+    // }
 
-    @DeleteMapping("/like/{id}")
-    @Operation(summary = "특정 즐겨찾기 삭제", description = "특정 즐겨찾기를 삭제합니다.")
-    public ResponseEntity<Like> deleteLike(@PathVariable Long id) {
-        likeRepository.deleteById(id);
-        // if (reservation == null) {
-        // return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        // }
-        return new ResponseEntity<Like>(HttpStatus.OK);
-    }
+    // @DeleteMapping("/like/{id}")
+    // @Operation(summary = "특정 예약정보 삭제", description = "특정 예약정보를 삭제합니다.")
+    // public ResponseEntity<Reservation> deleteReservation(@PathVariable String id)
+    // {
+    // Reservation reservation = reservationRepository.findByEmail(id);
+
+    // if (reservation == null) {
+    // return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    // }
+    // reservationRepository.delete(reservation);
+
+    // return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    // }
 
 }
