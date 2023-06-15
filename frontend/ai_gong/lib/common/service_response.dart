bb04db38e027c007ee91757729295964737a2c44
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-06-15 22:03:26 +0900

    update comment

--- frontend/ai_gong/lib/common/service_response.dart
+++ frontend/ai_gong/lib/common/service_response.dart
@@ -1,3 +1,7 @@
+// Summary:
+// Api response class for handling api response
+// This class is used to check if the response is successful
+
 class ApiResponse<T> {
   ApiResponse({required this.result, this.value, this.errorMsg = ''});
 
--- frontend/ai_gong/lib/common/title.dart
+++ frontend/ai_gong/lib/common/title.dart
@@ -1,3 +1,5 @@
+// Summary:
+//  This class is used to store page titles
 class PageTitle {
   static const String main = '애공 : AI-gong';
 }
